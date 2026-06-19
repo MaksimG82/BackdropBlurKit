b8e864088e6de67f705c1fc80d41208d8802665f
Maksim Gaisin <maksim@gaisin.space>
2026-06-19 16:32:12 +0200

    Example app added

---
+++ Example/BackdropBlurKitExampleApp/BackdropBlurKitExampleApp/BackdropBlurKitExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackdropBlurKitExampleAppApp.swift
+//  BackdropBlurKitExampleApp
+//
+//  Created by Maksim Gaisin on 19.06.26.
+//
+
+import SwiftUI
+
+@main
+struct BackdropBlurKitExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
