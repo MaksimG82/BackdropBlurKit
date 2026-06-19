268c255e4c9a0d3879b092589b5b7dcf07e8a2d7
Maksim Gaisin <maksim@gaisin.space>
2026-06-19 19:43:42 +0200

    example app updated

--- Example/BackdropBlurKitExampleApp/BackdropBlurKitExampleApp/ContentView.swift
+++ Example/BackdropBlurKitExampleApp/BackdropBlurKitExampleApp/ContentView.swift
@@ -10,25 +10,38 @@ import BackdropBlurKit
 
 struct ContentView: View {
     var body: some View {
-            ZStack {
-                
-                LinearGradient(colors: [.red, .blue, .green], startPoint: .topLeading, endPoint: .bottomTrailing)
-                    .ignoresSafeArea()
+        ZStack {
+            LinearGradient(colors: [.red, .blue, .green], startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea()
 
-                ScrollView {
-                    VStack(spacing: 20) {
-                        ForEach(0..<2000) { i in
-                            Text("Item \(i)")
-                                .padding()
-                                .background(.ultraThinMaterial)
-                                .cornerRadius(10)
-                        }
+            ScrollView {
+                VStack(spacing: 20) {
+                    ForEach(0..<2000) { i in
+                        Text("Item \(i)")
+                            .padding()
+                            .background(.ultraThinMaterial)
+                            .cornerRadius(10)
                     }
-                    .padding()
                 }
-                .blurSource()
+                .padding()
+            }
+
+            VStack(spacing: 40) {
+                Text("Floating Element 1")
+                    .padding(30)
+                    .background(.white.opacity(0.9))
+                    .cornerRadius(15)
+//                    .blurred(cornerRadius: 15)
+
+                Text("Floating Element 2")
+                    .padding(40)
+                    .background(.white.opacity(0.9))
+                    .cornerRadius(30)
+//                    .blurred(cornerRadius: 30)
             }
         }
+//        .blurSource()
+    }
 }
 
 #Preview {
